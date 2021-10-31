2d9e1608b6bb844c93ddfde8e1534f4c08e79eac
GabrielStancu <gabriel.stancu07@yahoo.com>
2021-10-31 17:54:34 +0200

    Added core functionality

---
+++ Testing/Testing/MainTestings.cs
@@ -0,0 +1,9 @@
+﻿namespace Testing
+{
+    public class MainTestings
+    {
+        public static string Text1 = "Change this!";
+        public static string Text2 = "Change this!";
+        public static string Text3 = "Change this!";
+    }
+}
